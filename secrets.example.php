7aeadba20d1851595a5d8dca8ef9b063089c56e5
Cristian Herrera <cherreragiraldo@gmail.com>
2022-08-12 16:25:09 -0400

    add shell script and further docs on sample file

--- secrets.example.php
+++ secrets.example.php
@@ -1,10 +1,25 @@
 <?php
 
 return [
-    'DB_PASSWORD' => '',
-    'DB_NAME' => '',
-
+    /* AWS Credentials */
     'AWS_ACCESS_KEY' => '',
     'AWS_SECRET_ACCESS_KEY' => '',
-    'AWS_BUCKET' => '',
+    'AWS_REGION' => 'us-east-1',
+
+    /* Database - This would be handled by RDS */
+    'DB_HOST' => '',
+    'DB_USER' => '',
+    'DB_PASSWORD' => '',
+    'DB_NAME' => 'app',
+
+    /* File Storage and Delivery */
+    'AWS_BUCKET' => 'my-serverless-app-bucket',
+    'CLOUDFRONT_URI' => 'https://your-cloudfront-uri.com',
+
+    /* Redis - Handled by ElastiCache */
+    /* Using this we replace PHP's native session handler */
+    /* @see \LifeSpikes\Support\Redis::handleSessions() */
+    'REDIS_HOST' => 'localhost',
+    'REDIS_PORT' => 6379,
+    'REDIS_PASSWORD' => '',
 ];
